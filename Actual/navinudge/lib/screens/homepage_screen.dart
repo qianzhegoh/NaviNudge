e785dd5b2ef82cefba6cc808d600664a49ce65fd
Pan Ziyue <random.rrr3@gmail.com>
2024-08-03 12:12:53 +0800

    refactored out the home page into its own file

---
+++ Actual/navinudge/lib/screens/homepage_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'bluetooth_setup_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(1.3412841720874797, 103.96375602825361);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  List<Widget> _pages() => <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 17.0,
+          ),
+        ),
+        ProfilePage(),
+      ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        _pages().elementAt(_selectedIndex),
+        Positioned(
+            bottom: 60,
+            left: MediaQuery.of(context).size.width / 2 - 80,
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NavigationPage()));
+              },
+              icon: Icon(Icons.play_arrow),
+              label: Text("Start Navigation")
+            ))
+      ]),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Please choose your destination'),
+        ),
+      body: Center(
+        child: Column(
+          children: [SizedBox(
+            height:50,
+            width: 300,
+            child: ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => BluetoothSetup())
+                );
+              }, 
+              child: const Text("Bluetooth example",
+                        style: TextStyle(fontSize: 18)),))],
+          ),
+      )
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text('Profile Content'),
+          ],
+        ),
+      ),
+    );
+  }
+}
