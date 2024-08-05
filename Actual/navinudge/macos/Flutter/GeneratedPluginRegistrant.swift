9e0aa74029e6783ad227bb6c470e9f881b39a720
Pan Ziyue <random.rrr3@gmail.com>
2024-08-05 12:39:18 +0800

    modified merge to eliminate errors

--- Actual/navinudge/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Actual/navinudge/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import flutter_blue_plus
 import geolocator_apple
-import location
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
